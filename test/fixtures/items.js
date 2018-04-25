7b45cc78411e6286ba9f51cf3941eb8843ad7993
Yingbiao Wang <yingbiao2000@yahoo.com>
2018-04-25 21:35:07 +1000

    Working on:
    Completed the following:
    * Single Author Chinese
    * Single Author English
    * Single Author with Translator
    * Single Editor Chinese
    * Single Editor English

---
+++ test/fixtures/items.js
@@ -0,0 +1,116 @@
+'use strict';
+
+module.exports = {
+    singleAuthorChinese: {
+        "id": "ITEM-1",
+        "title": "泪眼先知耶利米",
+        "author": [
+            {
+                "family": "杨",
+                "given": "牧谷"
+            }
+        ],
+        "publisher": "校园书房出版社",
+        "publisher-place": "台北",
+        "language":"zh-CN",
+        "issued": {
+            "date-parts": [
+                [
+                    1989
+                ]
+            ]
+        },
+        "type": "book"
+    },
+    singleAuthorEnglish: {
+        "id": "ITEM-2",
+        "title": "The Clash of Civilizations",
+        "author": [
+            {
+                "family": "Huntington",
+                "given": "Samuel P."
+            }
+        ],
+        "publisher": "Touchstone Books",
+        "publisher-place": "London",
+        "language":"en-US",
+        "issued": {
+            "date-parts": [
+                [
+                    1998
+                ]
+            ]
+        },
+        "type": "book"
+    },
+    singleAuthorWithTranslatorChinese: {
+        "id": "ITEM-3",
+        "title": "历史学",
+        "author": [
+            {
+                "family": "博",
+                "given": "能汉"
+            }
+        ],
+        "translator": [
+            {
+                "family": "黄",
+                "given": "晓兰",
+            }
+        ],
+        "publisher": "天天出版社",
+        "publisher-place": "香港",
+        "language":"zh-CN",
+        "issued": {
+            "date-parts": [
+                [
+                    2003
+                ]
+            ]
+        },
+        "type": "book"
+    },
+    singleEditorChinese: {
+        "id": "ITEM-4",
+        "title": "旧约小品",
+        "editor": [
+            {
+                "family": "李",
+                "given": "三谷"
+            }
+        ],
+        "publisher": "校园出版社",
+        "publisher-place": "台北",
+        "language":"zh-CN",
+        "issued": {
+            "date-parts": [
+                [
+                    2003
+                ]
+            ]
+        },
+        "type": "book"
+    },
+    singleEditorEnglish: {
+        "id": "ITEM-5",
+        "title": "The Love of Brothers",
+        "editor": [
+            {
+                "family": "Headman",
+                "given": "David",
+                "static-ordering": false
+            }
+        ],
+        "publisher": "Evergreen Books",
+        "publisher-place": "London",
+        "language":"en-US",
+        "issued": {
+            "date-parts": [
+                [
+                    2003
+                ]
+            ]
+        },
+        "type": "book"
+    }   
+}
